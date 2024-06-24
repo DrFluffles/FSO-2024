e49697bb46b19ac75a3de46ccecc7ef60b8b50ef
MIchaelDoan <michael.doan18@gmail.com>
2024-06-24 18:13:12 +1000

    Changing file structure

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,55 +1,80 @@
-const Header = (course) =>{
-  return <h1>{course.name}</h1>
-}
-const Content = (props) => {
-  return <div> 
-    <Part name = {props.part1} exercise = {props.exercises1}/>
-    <Part name = {props.part2} exercise = {props.exercises2}/>
-    <Part name = {props.part3} exercise = {props.exercises3}/>
+import { useState } from 'react'
+
+const Header = ({name}) => {
+  return (
+  <div>
+  <p>{name}</p>
   </div>
+  )
 }
 
-const Part = (part) =>{
-  return <p>{part.name}{part.exercise}</p>
-}
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 
-const Total = (prop) => {
-  return <p> Number of exercises {prop.one + prop.two + prop.three}</p> 
-}
-
-
+const Total = (props) =>(
+  <div>
+    <p>Good: {props.totalGood}</p>
+    <p>Neutral: {props.totalNeutral}</p>
+    <p>Bad: {props.totalBad}</p>
+    <p>All: {props.all}</p>
+  </div>
+)
 
 const App = () => {
-  
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good +1)
+    console.log(good);
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1) 
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1) 
+  }
+
+  const randomNumberButton = () => {
+    let number = Math.floor((Math.random() * 7) + 1)
+    setSelected(number)
+    console.log(selected)
   }
 
   return (
-    <div>
-      <Header name = {course.name}/>
-      <Content 
-      part1 = {course.parts[0].name} exercises1 = {course.parts[0].exercises}
-      part2 = {course.parts[1].name} exercises2 = {course.parts[1].exercises}
-      part3 = {course.parts[2].name} exercises3 = {course.parts[2].exercises}/>
-      <Total one = {course.parts[0].exercises} two = {course.parts[1].exercises} three = {course.parts[2].exercises}/>
-    </div>
+    
+      <div>
+      <Header name = "Fuckkks" />
+      <Button onClick = {handleGoodClick} text='good' />
+      <Button onClick = {handleNeutralClick} text='neutral' />
+      <Button onClick = {handleBadClick} text='bad' />
+      <Button onClick = {randomNumberButton} text = 'Random quote'/>
+      <Total totalGood = {good} totalNeutral = {neutral} totalBad = {bad} all ={good + neutral + bad}/>
+      
+      {anecdotes[selected]}
+      </div>
   )
 }
+
 
 export default App
