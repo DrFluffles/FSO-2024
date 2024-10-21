f6204956a1d9926befc5b2d9053319453fb7611c
MIchaelDoan <michael.doan18@gmail.com>
2024-10-21 16:58:38 +1100

    Everything before 3.13.-3.14.

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
+const Note = require('./models/note')
+require("dotenv").config()
+
+const mongoose = require('mongoose')
+
+
+//const password = process.argv[2];
+//const encodedPassword = encodeURIComponent(password);
+
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+
 
 let notes = [
   {
@@ -45,7 +56,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 const generateId = () => {
@@ -58,32 +71,24 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    console.log('x')
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -95,7 +100,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
