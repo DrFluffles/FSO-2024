8ecb2e08b35395ef55db65be7e506bc5fb8e169e
MIchaelDoan <michael.doan18@gmail.com>
2025-02-23 20:06:24 +1100

    Completion of Chapter 4 for notes

--- part3/app.js
+++ part3/app.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
+const loginRouter = require('./controllers/login')
 const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
 
 mongoose.set('strictQuery', false)
 
@@ -22,9 +27,13 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
-app.use(middleware.requestLogger)
 
+app.use(middleware.requestLogger)
+app.use('/api/login', loginRouter)
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
+
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
